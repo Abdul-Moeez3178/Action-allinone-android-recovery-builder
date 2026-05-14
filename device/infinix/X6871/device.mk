7fd1f44128a4d4219e4e55244dee5d37ce199699
Abdul-Moeez3178 <162436577+Abdul-Moeez3178@users.noreply.github.com>
2026-05-14 11:58:18 +0100

    Add device.mk base configuration for Infinix-X6871

---
+++ device/infinix/X6871/device.mk
@@ -0,0 +1,15 @@
+#
+# Device Configuration for Infinix X6871
+#
+
+LOCAL_PATH := device/infinix/X6871
+
+# Inherit from AOSP
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Device properties
+PRODUCT_BRAND := infinix
+PRODUCT_DEVICE := X6871
+PRODUCT_NAME := pbrp_X6871
+PRODUCT_MODEL := Infinix-X6871
+PRODUCT_MANUFACTURER := infinix
